3f7d529f39a5870f0b66c9cedff213f0ac11cd78
Tri Pham <tripham@Tris-MacBook-Pro.local>
2023-06-14 12:11:30 -0500

    update portfolio

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,7 @@ class Projects extends Component {
                   <img
                     src={projects.images[0]}
                     alt="projectImages"
-                    height="330"
+                    width="100%"
                     style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
                   />
                   <span className="project-date">{projects.startDate}</span>
