0ce0e60eb01e417a26566f604947ffe33e97b047
Tri Pham <minhtri2805@gmail.com>
2023-06-14 13:58:33 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,8 +31,6 @@ class Projects extends Component {
                   <img
                     src={projects.images[0]}
                     alt="project Images"
-                    width="100%"
-                    height="250px"
                     style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
                   />
                   <span className="project-date">{projects.startDate}</span>
