3ac33b2e66d4f59b50d7d8bf3bfa3e11f67906b4
Tri Pham <tripham@Tris-MacBook-Pro.local>
2023-06-14 13:40:25 -0500

    design: resize

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,8 +31,8 @@ class Projects extends Component {
                   <img
                     src={projects.images[0]}
                     alt="projectImages"
-                    width="100%"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative', height: 'auto'}}
+                    // width="100%"
+                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
                   />
                   <span className="project-date">{projects.startDate}</span>
                   <br />
